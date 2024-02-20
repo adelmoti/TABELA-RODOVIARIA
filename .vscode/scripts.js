0d35464e3ebf753597a7546020b2ac73231c5986
Adelmo <adelmopotenca@hotmail.com>
2024-02-20 19:34:57 -0300

    update
    atualização

---
+++ .vscode/scripts.js
@@ -0,0 +1,10 @@
+
+            function Evento_adiciona()
+            {
+                alert('Item Adicionado com Sucesso !')
+            }
+            function Evento_remover()
+            {
+                alert('Item Removido com Sucesso !')
+            }
+      
